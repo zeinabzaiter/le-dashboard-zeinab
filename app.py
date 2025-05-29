cd9f1262ea29033d575a2b5834092b4829f8951b
zeinabzaiter <zeinab.zeaiter@etu.univ-amu.fr>
2025-05-29 15:19:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard AB", layout="wide")
+
+st.title("📊 Dashboard Résistance Antibiotiques")
+st.markdown("Utilisez le menu à gauche pour naviguer entre les onglets.")
